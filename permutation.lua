3c645b1db7a1f17aa01d3cd89093c09f1f2483cb
saka1029 <saka1029@gmail.com>
2025-11-01 19:30:56 +0900

    ..

--- permutation.lua
+++ permutation.lua
@@ -1,4 +1,4 @@
-function array(n, v)
+function array_new(n, v)
     local a = {}
     for i = 1, n do
         table.insert(a, v)
@@ -6,10 +6,18 @@ function array(n, v)
     return a
 end
 
-function permutation(n, k, callback)
-    print("permutation(" .. n .. ", " .. k .. ")")
-    local result = array(k, 0)
-    local used = array(n, false)
+function array_print(a) 
+    local s = "";
+    for _, v in pairs(a) do
+        s = s .. " " .. v
+    end
+    print(string.sub(s, 2))
+end
+
+function permutation_recursive_callback(n, k, callback)
+    print("permutation_recursive_callback(" .. n .. ", " .. k .. ", callback)")
+    local result = array_new(k, 0)
+    local used = array_new(n, false)
     local function solve(i)
         if i > k then
             callback({table.unpack(result)})
@@ -27,13 +35,36 @@ function permutation(n, k, callback)
     solve(1)
 end
 
-function pr(r) 
-    local s = "";
-    for i, v in pairs(r) do
-        s = s .. " " .. v
+function permutation_recursive_array(n, k)
+    print("permutation_recursive_array(" .. n .. ", " .. k .. ")")
+    local ret = {}
+    local result = array_new(k, 0)
+    local used = array_new(n, false)
+    local function solve(i)
+        if i > k then
+            table.insert(ret, {table.unpack(result)})
+        else
+            for j = 1, n do
+                if not used[j] then
+                    used[j] = true
+                    result[i] = j
+                    solve(i + 1)
+                    used[j] = false
+                end
+            end
+        end
     end
-    print(s)
+    solve(1)
+    return ret
 end
 
-print("** start **")
-permutation(3, 3, pr)
+permutation_recursive_callback(3, 3, array_print)
+
+local result = permutation_recursive_array(3, 3)
+for _, v in pairs(result) do
+    local s = "";
+    for _, w in pairs(v) do
+        s = s .. " " .. w
+    end
+    print(string.sub(s, 2))
+end
