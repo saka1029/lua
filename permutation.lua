8564cb0aa7ed9470f41f967d204ed40dfc848f27
saka1029 <saka1029@gmail.com>
2025-11-02 10:41:25 +0900

    iterator

--- permutation.lua
+++ permutation.lua
@@ -35,6 +35,8 @@ function permutation_recursive_callback(n, k, callback)
     solve(1)
 end
 
+permutation_recursive_callback(3, 3, array_print)
+
 function permutation_recursive_array(n, k)
     print("permutation_recursive_array(" .. n .. ", " .. k .. ")")
     local ret = {}
@@ -58,8 +60,6 @@ function permutation_recursive_array(n, k)
     return ret
 end
 
-permutation_recursive_callback(3, 3, array_print)
-
 local result = permutation_recursive_array(3, 3)
 for _, v in pairs(result) do
     local s = "";
@@ -67,4 +67,39 @@ for _, v in pairs(result) do
         s = s .. " " .. w
     end
     print(string.sub(s, 2))
+end
+
+-- iteratorが再帰呼出しするとうまく行かない。
+-- iteratorがreturnではなくyieldで値を返せばうまく行くのではないか？
+function permutation_recursive_iterator(n, k)
+    print("permutation_recursive_iterator(" .. n .. ", " .. k .. ")")
+    local result = array_new(k, 0)
+    local used = array_new(n, false)
+    return function() 
+        print("iterator")
+        local function solve(i)
+            print("sove(" .. i .. ")")
+            if i > k then
+                local r = {table.unpack(result)}
+                array_print(r)
+                return r
+            else
+                for j = 1, n do
+                    if not used[j] then
+                        used[j] = true
+                        result[i] = j
+                        solve(i + 1)
+                        used[j] = false
+                    end
+                end
+            end
+        end
+        solve(1)
+        return nil
+    end
+end
+
+for a in permutation_recursive_iterator(3, 3) do
+    print(a)
+    array_print(a)
 end
