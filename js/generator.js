cbee6b7ac495916e66b57662db70f3db8d7efb54
saka1029 <saka1029@gmail.com>
2025-11-04 08:39:49 +0900

    ..

--- js/generator.js
+++ js/generator.js
@@ -1,8 +1,25 @@
 function * generator() {
-    yield 3
-    yield 1
-    yield 2
+    yield 3;
+    yield 1;
+    yield 2;
 }
 
+console.log("** generator **");
 for (var i of generator())
-    console.log(i)
+    console.log(i);
+
+function * fib() {
+    var a = 0, b = 1;
+    while (true) {
+        yield a;
+        [a, b] = [b, a + b];
+    }
+}
+
+console.log("** fib **");
+var c = 0;
+for (var i of fib()) {
+    if (c++ > 100)
+        break;
+    console.log(i);
+}
